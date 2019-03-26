c66348c052652da3d077e0105188c66c30a451b4
njorogepeter <pgakonde@yahoo.com>
2019-03-26 15:40:16 -0700

    added friends

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+        "name": "Rachel",
+        "photo": "https://www.imdb.com/name/nm0000098/mediaviewer/rm226134272",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Monica",
+        "photo":"https://www.imdb.com/name/nm0001073/mediaviewer/rm651071232",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Phoebe",
+        "photo":"https://www.imdb.com/name/nm0001435/mediaviewer/rm1084919552",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Joey",
+        "photo":"https://www.imdb.com/name/nm0001455/mediaviewer/rm1082698240",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Chandler",
+        "photo":"https://www.imdb.com/name/nm0001612/mediaviewer/rm261783552",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Ross",
+        "photo":"https://www.imdb.com/name/nm0001710/?ref_=ttfc_fc_cl_i6",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Gunther",
+        "photo":"https://www.imdb.com/name/nm0878837/?ref_=ttfc_fc_cl_i7",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+        
+    },
+    {
+        "name": "Jack",
+        "photo":"https://www.imdb.com/name/nm0001285/?ref_=ttfc_fc_cl_i8",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Judy",
+        "photo": "https://www.imdb.com/name/nm0681943/mediaviewer/rm1693664512",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Janice",
+        "photo":"https://www.imdb.com/name/nm0923909/mediaviewer/rm1948813056",
+        "scores":[5,1,4,4,5,1,2,5,4,1]
+    }
+]
