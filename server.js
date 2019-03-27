aaecebb62a9e0fa26985f21433ea5999f0ee93ae
njorogepeter <pgakonde@yahoo.com>
2019-03-27 10:04:02 -0700

    changes

--- server.js
+++ server.js
@@ -9,3 +9,10 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var apiRoutes = require("./app/routing/apiRoutes.js");
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
+
+app.listen(PORT, function(){
+    console.log("App is listening at your port");
+});
